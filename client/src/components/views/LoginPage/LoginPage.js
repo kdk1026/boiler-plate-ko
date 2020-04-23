cd97bd3ab355a25e8b6ffb4cdcec1be630ab19b7
kdk1026 <kdk1026@gmail.com>
2020-04-23 12:26:40 +0900

    회원가입, 로그아웃, 인증체크

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../../../_actions/user_action';
+import { withRouter } from 'react-router-dom';
 
 function LoginPage(props) {
     const dispatch = useDispatch();
@@ -56,4 +57,4 @@ function LoginPage(props) {
     )
 }
 
-export default LoginPage
+export default withRouter(LoginPage)
